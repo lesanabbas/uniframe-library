a2d54c95f10f2d3b5ba0ee886f294174104d4c0d
Lesan Abbas <63160640+lesanabbas@users.noreply.github.com>
2025-02-22 12:21:21 +0530

    Link tag changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
 "use client"
-import Image from "next/image";
-import { useState } from 'react';
 import BuildYourDesign from "@/components/BuildYourDesign";
 import BuildForDeveloper from "@/components/BuildForDeveloper";
 
